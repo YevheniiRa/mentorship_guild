ac49a71c945f4f9c048a61fe4111c1949bb67860
unknown <56446445+Matsenko@users.noreply.github.com>
2020-08-03 10:22:23 +0300

    db

--- flaskr/__init__.py
+++ flaskr/__init__.py
@@ -1,17 +1,19 @@
 import os
-
+from . import db
 from flask import Flask
+from . import auth
 
 
 def create_app(test_config=None):
     # create and configure the app
     app = Flask(__name__, instance_relative_config=True)
+    db.init_app(app)
     app.config.from_mapping(
         SECRET_KEY='dev',
         DATABASE=os.path.join(app.instance_path, 'flaskr.sqlite'),
     )
-
     if test_config is None:
+
         # load the instance config, if it exists, when not testing
         app.config.from_pyfile('config.py', silent=True)
     else:
@@ -24,7 +26,6 @@ def create_app(test_config=None):
     except OSError:
         pass
 
-    from . import auth
-    app.register_blueprint(auth.bp)
+  #  app.register_blueprint(auth.bp)
 
     return app
