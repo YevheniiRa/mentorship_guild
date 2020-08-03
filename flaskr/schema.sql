efc352bc121e0804aff5fa97be4da81c679a505c
unknown <56446445+Matsenko@users.noreply.github.com>
2020-08-03 11:01:39 +0300

    schema.sql

--- flaskr/schema.sql
+++ flaskr/schema.sql
@@ -1,30 +1,29 @@
 # Matsenko
-
-DROP TABLE IF EXISTS user;
+DROP TABLE IF EXISTS user_tab; 
 DROP TABLE IF EXISTS post;
 
-CREATE TABLE user (
-  id INTEGER PRIMARY KEY AUTOINCREMENT,
+CREATE TABLE user_tab (
+  id  INTEGER  PRIMARY KEY,  
   username TEXT UNIQUE NOT NULL,
   password TEXT NOT NULL
 );
 
 CREATE TABLE post (
-  id INTEGER PRIMARY KEY AUTOINCREMENT,
+  id INTEGER  PRIMARY KEY ,
   author_id INTEGER NOT NULL,
   created TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
   title TEXT NOT NULL,
   body TEXT NOT NULL,
-  FOREIGN KEY (author_id) REFERENCES user (id)
+  FOREIGN KEY (author_id) REFERENCES user_tab (id)
 );
 
 
-CREATE TABLE cource (
+CREATE TABLE course (
     name TEXT NOT NULL,
     start_date DATE,
-    FOREIGN KEY (author_id) REFERENCES user (id)
-    description TEXT NOT NULL
-    studends_ids TEXT NOT NULL 
-)
+    FOREIGN KEY (author_id) REFERENCES user_tab (id),
+    description TEXT NOT NULL,
+    studends_id TEXT NOT NULL 
+);
 
 -- JSON([1, 2, 3, 4])
